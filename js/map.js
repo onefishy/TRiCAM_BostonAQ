786242816d156ab13fe9d4856e9ffeba880b83da
Tara Ippolito <taraippolito@Taras-MacBook-Pro.local>
2017-07-21 10:53:29 -0400

    fixes WORKING 7/21

--- js/map.js
+++ js/map.js
@@ -51,17 +51,32 @@ function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
 
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
+$(".program").on('click',function () {
+  $('li div ul').toggle('');
+});
+$('.nav > li div ul li a').click(function(e) {
+  e.stopPropagation();
+});
+$('.nav2 > li div ul li a').click(function(e) {
+  e.stopPropagation();
+});
+$('.nav3 > li div ul li a').click(function(e) {
+  e.stopPropagation();
+});
 
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
+// Close the dropdown menu if the user clicks outside of it
+// window.onclick = function(event) {
+//   if (!event.target.matches('.dropbtn')) {
+
+//     var dropdowns = document.getElementsByClassName("dropdown-content");
+//     var i;
+//     for (i = 0; i < dropdowns.length; i++) {
+//       var openDropdown = dropdowns[i];
+//       if (openDropdown.classList.contains('show')) {
+//         openDropdown.classList.remove('show');
+//       }
+//     }
+//   }
+// }
+
+
